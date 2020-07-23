2d63384c320e24a79c03df7ec26caf29a597a825
CodingQuell <ryequellhunter@gmail.com>
2020-07-23 16:26:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+passcode = str(input("Enter a custom passcode:  "))
+
+
+
+
+c = "Quell/1204"
+found = False
+while found == False:
+  d = str(input("Please enter your 8-16 digit passcode"))
+  if d==c:
+    found = True
+    print("access granted")
+else:
+  print("Wrong passcode")
+
+
+
